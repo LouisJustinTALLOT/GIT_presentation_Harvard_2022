ec41d1388892216b73edfed1e893aa1e98b5730b
Louis-Justin TALLOT <lj.tallot@gmail.com>
2022-07-29 12:03:56 -0400

    Finish text user interface

--- script.py
+++ script.py
@@ -2,5 +2,10 @@ import computation
 
 print("Hello Harvard and the world !")
 
+
+
+print("I'm editing on Vim")
+print("But I prefer VS Code")
+
 print("Using the add function : ")
 print(computation.add(1, 2))
