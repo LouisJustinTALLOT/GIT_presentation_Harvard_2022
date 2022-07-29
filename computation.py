2e737f27bcd58f4d73809e026a1ecdd245670b02
Florent Pollet <florent.pollet@mines-paristech.fr>
2022-07-29 13:16:21 -0400

    Add Multiplication Feature

--- computation.py
+++ computation.py
@@ -6,3 +6,7 @@ def add(a: float, b: float) -> float:
 def sub(a: float, b: float) -> float:
     """Subtract the two inputs"""
     return a - b
+
+def mul(a: float, b: float) -> float:
+    """Multiply the two inputs"""
+    return a * b
